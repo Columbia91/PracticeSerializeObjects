d4cf596f628fa3d54a5ec22bf1aacebf603b731d
cougar91 <alpha.z91@mail.ru>
2019-03-10 15:54:30 +0600

    Добавьте файлы проекта.

---
+++ ClassLib/PC.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLib
+{
+    public class PC
+    {
+        public string Brand { get; set; }
+        public string SerialNumber { get; set; }
+        public string CPU { get; set; }
+
+        public PC()
+        {
+
+        }
+        public PC(string brand, string number, string cpu)
+        {
+            Brand = brand; SerialNumber = number; CPU = cpu;
+        }
+
+        public void SwitchOnOff()
+        {
+            Console.WriteLine("Метод включения/выключения ПК");
+        }
+        public void Restart()
+        {
+            Console.WriteLine("Метод перезагрузки ПК");
+        }
+    }
+}
+/*■	3–4 поля (марка, серийный номер и т.д.),  
+■	свойства (к каждому полю),  
+■	конструкторы (по умолчанию, с параметрами),  
+■	методы, моделирующие функционирование ПЭВМ (включение/выключение, перегрузку). 
+○	Создать новый проект (тип — консольное приложение) с именем «SerializConsolApp». Добавить ссылку на библиотеку «ClassLib».
+*/
---
+++ DeserializeConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLib;
+
+
+namespace DeserializeConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<PC> pcList = new List<PC>();
+            Console.WriteLine(path);
+
+        }
+    }
+}
